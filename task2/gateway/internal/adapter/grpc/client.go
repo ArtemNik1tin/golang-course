98d4be48bddd3015ca137a4b38745b5a2f2c2ab8
ArtemNik1tin <artemnikitin24453@gmail.com>
2026-03-22 22:31:37 +0300

    feat: add grpc client

---
+++ task2/gateway/internal/adapter/grpc/client.go
@@ -0,0 +1,40 @@
+package grpc
+
+import (
+	"context"
+	"fmt"
+
+	pb "github.com/ArtemNik1tin/distributed-github/api/proto"
+	"github.com/ArtemNik1tin/distributed-github/gateway/internal/domain"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+type CollectorClient struct {
+	grpcClient pb.CollectorServiceClient
+}
+
+func NewCollectorClient(address string) (*CollectorClient, error) {
+	conn, err := grpc.NewClient(address, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		return nil, fmt.Errorf("failed to connect: %w", err)
+	}
+	return &CollectorClient{
+		grpcClient: pb.NewCollectorServiceClient(conn),
+	}, nil
+}
+
+func (client CollectorClient) Fetch(ctx context.Context, ownerName string, repoName string) (*domain.Repository, error) {
+	grpcRequest := &pb.RepositoryRequest{OwnerName: ownerName, RepoName: repoName}
+	response, err := client.grpcClient.GetRepository(ctx, grpcRequest)
+	if err != nil {
+		return nil, fmt.Errorf("GetRepository error: %w", err)
+	}
+	return &domain.Repository{
+		Name:        response.Name,
+		Description: response.Description,
+		Stars:       int(response.Stars),
+		Forks:       int(response.Forks),
+		CreatedAt:   response.CreatedAt,
+	}, nil
+}
